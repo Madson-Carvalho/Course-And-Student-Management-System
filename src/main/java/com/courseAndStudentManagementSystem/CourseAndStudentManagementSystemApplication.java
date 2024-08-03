a673a4ab5ee1329d321425809a1861e46ac11610
Madson-Carvalho <madsonraynnon@gmail.com>
2024-08-03 19:25:05 -0300

    Criando a base da aplicação.

---
+++ src/main/java/com/courseAndStudentManagementSystem/CourseAndStudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.courseAndStudentManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAndStudentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseAndStudentManagementSystemApplication.class, args);
+	}
+
+}
